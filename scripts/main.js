cdb3384aedd5768b7b5ed65ecd925f2469a0ce63
myinglin2333 <meiye.l@northeastern.edu>
2026-01-16 22:36:51 -0500

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,40 @@
+// Store a reference to the <h1> in a variable
+//const myHeading = document.querySelector("h1");
+// Update the text content of the <h1>
+//myHeading.textContent = "Hello world!";
+
+const myImage = document.querySelector("img");
+
+myImage.addEventListener("click", () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/mewo.png") {
+    myImage.setAttribute("src", "images/mewo2.jpg");
+  } else {
+    myImage.setAttribute("src", "images/mewo.png");
+  }
+});
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Hello ${myName}`;
+  }
+}
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Hello ${storedName}`;
+}
+
+// Add click event
+myButton.addEventListener("click", () => {
+  setUserName();
+});
